8fcfee427a3f5b9d1848a8e124962ef540f3398a
TechDaniels <128913507+TechDanielss@users.noreply.github.com>
2025-12-30 09:37:16 +0100

    Update scrpit.js

--- scrpit.js
+++ scrpit.js
@@ -1,6 +1,6 @@
 let user = document.getElementById('username');
 let btn = document.getElementById('login-btn');
 
-btn.addEventListener('click', function (e) {
+btn.addEventListener('click', function () {
     alert(`Hello again, ${user.value}!`);
 });
